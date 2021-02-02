713287d5cfd79f2975c9d72c4d7a604a4da05ee9
NirGefen <75200747+NirGefen@users.noreply.github.com>
2021-02-02 15:33:11 +0200

    Update Mixed_Code.ino

--- Mixed_Code.ino
+++ Mixed_Code.ino
@@ -1,5 +1,9 @@
 #include "DHT.h"
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
 #define DHTPIN 2     
 
 #define DHTTYPE DHT11   
@@ -12,6 +16,9 @@
 DHT dht(DHTPIN, DHTTYPE);
 const int gasPin = A0; //GAS sensor output pin to Arduino analog A0 pin
 SFE_BMP180 pressure;
+RF24 radio(9, 10); // CE, CSN  
+
+const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
 
 
 void setup() 
@@ -29,6 +36,12 @@ void setup()
     Serial.println("BMP180 init fail (disconnected?)\n\n");
     while(1); // Pause forever.
   }
+
+
+  radio.begin();                  //Starting the Wireless communication
+radio.openWritingPipe(address); //Setting the address where we will send the data
+radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+radio.stopListening();          //This sets the module as transmitter
 }
 
 void loop() 
@@ -65,5 +78,8 @@ void loop()
           Serial.print(p0);
           Serial.println("hPa\n");
         }
+
+   const char text[] = "Hello!";
+    radio.write(&text, sizeof(text));                  //Sending the message to receiver
   delay(5000);
 }
