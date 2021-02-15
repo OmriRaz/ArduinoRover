686807392e16eff6a59f66387fd18f77a3097641
NirGefen <75200747+NirGefen@users.noreply.github.com>
2021-02-15 15:38:41 +0200

    Add files via upload

---
+++ PMS5003/PMS5003.ino
@@ -0,0 +1,62 @@
+#include "Adafruit_PM25AQI.h"
+ 
+// If your PM2.5 is UART only, for UNO and others (without hardware serial) 
+// we must use software serial...
+// pin #2 is IN from sensor (TX pin on sensor), leave pin #3 disconnected
+// comment these two lines if using hardware serial
+#include <SoftwareSerial.h>
+SoftwareSerial pmSerial(2, 3);
+ 
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+ 
+void setup() {
+  // Wait for serial monitor to open
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+ 
+  Serial.println("Adafruit PMSA003I Air Quality Sensor");
+ 
+  delay(1000); // boot up
+
+  pmSerial.begin(9600);
+
+  if (! aqi.begin_UART(&pmSerial)) { // connect to the sensor over software serial 
+    Serial.println("Could not find PM 2.5 sensor!");
+    while (1) delay(10);
+  }
+ 
+  Serial.println("PM25 found!");
+}
+ 
+void loop() {
+  PM25_AQI_Data data;
+  
+  if (! aqi.read(&data)) {
+    delay(500);  // failed reading, try again in a bit
+    return;
+  }
+ 
+  Serial.println();
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Concentration Units (standard)"));
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
+  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
+  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
+  Serial.println(F("Concentration Units (environmental)"));
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_env);
+  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_env);
+  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_env);
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("Particles > 0.3um / 0.1L air:")); Serial.println(data.particles_03um);
+  Serial.print(F("Particles > 0.5um / 0.1L air:")); Serial.println(data.particles_05um);
+  Serial.print(F("Particles > 1.0um / 0.1L air:")); Serial.println(data.particles_10um);
+  Serial.print(F("Particles > 2.5um / 0.1L air:")); Serial.println(data.particles_25um);
+  Serial.print(F("Particles > 5.0um / 0.1L air:")); Serial.println(data.particles_50um);
+  Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
+  Serial.println(F("---------------------------------------"));
+  
+ 
+  delay(1000);
+}
